bdfdef66c44add7669ba45dd489f18a417100839
Numeric <>
2017-10-16 23:21:49 -0400

    Remove boilerplate code

--- Spec/ColorViewController.swift
+++ Spec/ColorViewController.swift
@@ -22,24 +22,8 @@ class ColorViewController: UIViewController {
         colorLabel.textColor = color
         colorLabel.text = colorName
         self.navigationItem.title = colorName
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func handletap(_ sender: UITapGestureRecognizer) {
         if (!showColor) {
             self.view.backgroundColor = color
